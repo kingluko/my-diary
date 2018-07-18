a2ad2213311a96d7ce8ae415403194027f53862f
kingluko <kelvin.kitika@gmail.com>
2018-07-18 14:19:50 +0300

    Chore #159135012 Adding models before testing

---
+++ app/models.py
@@ -0,0 +1,28 @@
+import datetime
+
+
+
+class Users():
+    def create_user(self):
+        pass
+    
+    def view_profile(self):
+        pass
+
+
+
+
+class Entries():
+    def __init__(self):
+        pass
+    def add_entry(self):
+        pass
+
+    def modify_entry(self, id):
+        pass
+    
+    def get_entries(self):
+        pass
+
+    def get_one_entry(self, id):
+        pass
