6bc145c1d66686d1cdcfbb4f008a0786601b809a
kingluko <kelvin.kitika@gmail.com>
2018-07-25 21:39:08 +0300

    Added db connection method

--- app/db_connect.py
+++ app/db_connect.py
@@ -14,29 +14,39 @@ class DbConnect:
         except psycopg2.DatabaseError as error:
             print(error)
 
-    def create_users_table(self):
-        table_users = (
-            """
-            CREATE TABLE users(
-                ID SERIAL PRIMARY KEY,
-                FIRSTNAME VARCHAR(100) NOT NULL,
-                LASTNAME VARCHAR(100) NOT NULL,
-                EMAIL VARCHAR(100) UNIQUE NOT NULL,
-                PASSWORD VARCHAR(100) NOT NULL
-            )
-            """
-        )
-        self.cursor.execute(table_users)
+    # def read_table(self, table_nme):
+    #     self.connect.cursor.execute(f"SELECT * FRPM {table_name}")
 
-    def create_entries_table(self):
-        table_entries = (
-            """
-            CREATE TABLE entries(
-                ID SERIAL PRIMARY KEY,
-                DATE VARCHAR(100) NOT NULL
-                TITLE VARCHAR NOT NULL,
-                STORY VARCHAR NOT NULL
-            )
-            """
-        )
-        self.cursor.execute(table_entries)
+    # def insert (self, **params):
+    #     name = params.get("name")
+
+    #     self.connect.cursor.execute(f"SELECT * FRPM {table_name}")
+
+
+
+    # def create_users_table(self):
+    #     table_users = (
+    #         """
+            # CREATE TABLE users(
+            #     ID SERIAL PRIMARY KEY,
+            #     FIRSTNAME VARCHAR(100) NOT NULL,
+            #     LASTNAME VARCHAR(100) NOT NULL,
+            #     EMAIL VARCHAR(100) UNIQUE NOT NULL,
+            #     PASSWORD VARCHAR(100) NOT NULL
+            # )
+    #         """
+    #     )
+    #     self.cursor.execute(table_users)
+
+    # def create_entries_table(self):
+    #     table_entries = (
+    #         """
+    #         CREATE TABLE entries(
+    #             ID SERIAL PRIMARY KEY,
+    #             DATE VARCHAR(100) NOT NULL
+    #             TITLE VARCHAR NOT NULL,
+    #             STORY VARCHAR NOT NULL
+    #         )
+    #         """
+    #     )
+    #     self.cursor.execute(table_entries)
