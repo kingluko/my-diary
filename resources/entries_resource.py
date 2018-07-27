e520c210d44d2a2f951cb2277d686c3251872642
kingluko <kelvin.kitika@gmail.com>
2018-07-27 14:18:37 +0300

    Added Entry validation

--- resources/entries_resource.py
+++ resources/entries_resource.py
@@ -1,17 +1,33 @@
 from flask import jsonify
 from flask_restful import reqparse, Resource
+from app.models import Users, Entries
 
 
 class AllEntries(Resource):
     """Validates all entries on entry"""
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        'title',
+        required=True,
+        type=str,
+        trim=True,
+        help='Enter a valid title')
+
+    parser.add_argument(
+        'story',
+        required=True,
+        type=str,
+        trim=True,
+        help='Enter a valid text')
 
     def post(self):
         """Add an Entry"""
-        results = self.reqparse.parse_args()
+        results = AllEntries.parser.parse_args()
         title = results.get('title')
         story = results.get('story')
 
-
+        #g flask
+        # entry = (title=title, story=story)
 
     def get(self):
         """Fetch all Entries by user"""
