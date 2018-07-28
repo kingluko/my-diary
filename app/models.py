41b38f7acd9c98d9f3f13cbbe2861933fc802600
kingluko <kelvin.kitika@gmail.com>
2018-07-28 22:14:43 +0300

    Added Entry models

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 from passlib.hash import sha256_crypt
 from app import DbConnection
+from flask import jsonify
+import json
 
 db = DbConnection()
 
@@ -16,10 +18,53 @@ class Users:
     def signup_user(self):
         db.query(
             "INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)",
-            (self.name, self.email, self.username, self.password))    
-        
-        
-        
+            (self.name, self.email, self.username, self.password))
+
 
 class Entries:
+    def __init__(self, user_id, title, story):
+        self.user_id = user_id
+        self.title = title
+        self.story = story
+
+    def post(self):
+        """This method adds an entry into the database"""
+        db.query(
+            "INSERT INTO entries(user_id, title, story) VALUES(%s, %s, %s)",
+            (self.user_id, self.title, self.story))
+
+    @staticmethod
+    def get(user_id, entry_id=None):
+        """This method is used to get a single or all entries"""
+        if entry_id:
+            db.query(
+                "SELECT * FROM entries WHERE user_id = %s AND entry_id = %s",
+                (user_id, entry_id)
+            )
+            entries = db.cur.fetchall()
+            return entries
+        else:
+            db.query(
+                "SELECT * FROM entries WHERE user_id = %s", [user_id]
+            )
+            entry = db.cur.fetchall()
+            return entry
+
+    @staticmethod
+    def make_dict(entry_list):
+        entries = []
+        for entry in entry_list:
+            new_dict = {}
+            new_dict.update({
+                'title': entry[2],
+                'story': entry[3],
+                'date_created': entry[4].isoformat()
+                })
+            entries.append(new_dict)
+        return entries
     pass
+#TODO
+# create token decorators that will encode user id and store it together with entry
+
+
+Entries.get(user_id=11, entry_id=4)
