2f49b7d62168810233c0610ca25f94141b3d07ac
kingluko <kelvin.kitika@gmail.com>
2018-07-30 20:46:19 +0300

    Added config details to DbConnection class

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from flask_restful import Api, Resource
-from instance.config import config_app
+from app.instance.config import config_app
 import psycopg2
 import os
 
-if config_app['development']:
-    db_name = os.getenv('DATABASE_NAME')
-elif config_app['testing']:
+if config_app['testing']:
     db_name = os.getenv('DATABASE_TESTS')
+else:
+    db_name = os.getenv('DATABASE_NAME')
 
 db_user = os.getenv('DATABASE_USER')
 db_password = os.getenv('DATABASE_PASSWORD')
@@ -16,7 +16,15 @@ db_host = os.getenv('DATABASE_HOST')
 
 class DbConnection():
     """Initializes connection to the database and executes queries"""
-    def __init__(self):
+    def __init__(self, configuration=None):
+        if configuration == 'testing':
+            db_name = os.getenv('DATABASE_TESTS')
+        else:
+            db_name = os.getenv('DATABASE_NAME')
+        db_user = os.getenv('DATABASE_USER')
+        db_password = os.getenv('DATABASE_PASSWORD')
+        db_host = os.getenv('DATABASE_HOST')
+
         self.conn = psycopg2.connect(
             f"dbname={db_name} user={db_user} password={db_password} host={db_host}")
         self.conn.autocommit = True
@@ -37,10 +45,8 @@ def create_app(configuration):
     app = Flask(__name__, instance_relative_config=True)
     api = Api(app)
     app.config.from_object(config_app[configuration])
-    app.config.from_pyfile('config.py')
-
-    from resources.entries_resource import AllEntries, SingleEntry
-    from resources.user_resource import SigninResource, SignupResource
+    from app.resources.entries_resource import AllEntries, SingleEntry
+    from app.resources.user_resource import SigninResource, SignupResource
     # FIXME
     # use fstring to map url
     # url = "api/v1" 
