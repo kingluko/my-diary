6c8b97eab270c42d927dba20755e8f4bf146acbc
kingluko <kelvin.kitika@gmail.com>
2018-07-31 10:22:04 +0300

    Change file query

--- database.py
+++ database.py
@@ -4,16 +4,15 @@ db = DbConnection()
 
 
 def create_tables():
-    tables = (
+    db.query(
         """
         CREATE TABLE users(id serial PRIMARY KEY, name VARCHAR(100), email VARCHAR(100), username VARCHAR(30), password VARCHAR(100))
-        """,
+        """)
+    db.query(
         """
         CREATE TABLE entries(entry_id serial, user_id INTEGER REFERENCES users(id), title VARCHAR(200), story TEXT, date_created TIMESTAMP DEFAULT NOW()
         """
-    )
-    for table in tables:
-        db.query(table)
+    )    
 
 if __name__ == '__main__':
     create_tables()
