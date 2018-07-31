95f9b0b6e60faad44b64ce3ed74fedb77320cc89
kingluko <kelvin.kitika@gmail.com>
2018-07-31 10:18:46 +0300

    Remove Not exist from method

--- database.py
+++ database.py
@@ -6,10 +6,10 @@ db = DbConnection()
 def create_tables():
     tables = (
         """
-        CREATE TABLE IF NOT EXIST users(id serial PRIMARY KEY, name VARCHAR(100), email VARCHAR(100), username VARCHAR(30), password VARCHAR(100))
+        CREATE TABLE users(id serial PRIMARY KEY, name VARCHAR(100), email VARCHAR(100), username VARCHAR(30), password VARCHAR(100))
         """,
         """
-        CREATE TABLE IF NOT EXIST entries(entry_id serial, user_id INTEGER REFERENCES users(id), title VARCHAR(200), story TEXT, date_created TIMESTAMP DEFAULT NOW()
+        CREATE TABLE entries(entry_id serial, user_id INTEGER REFERENCES users(id), title VARCHAR(200), story TEXT, date_created TIMESTAMP DEFAULT NOW()
         """
     )
     for table in tables:
